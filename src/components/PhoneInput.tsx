710b0d75a8aaea279cc3ead1332e06d390e33b0d
drkedug@gmail.com <drkedug@gmail.com>
2024-05-30 16:06:13 -0300

    fix: phone input

--- src/components/PhoneInput.tsx
+++ src/components/PhoneInput.tsx
@@ -6,21 +6,32 @@ import { inputVariants } from './Input';
 export const PhoneInput = forwardRef<
   HTMLInputElement,
   Omit<Props, 'mask' | 'inputRef'>
->(({ className, ...props }, ref) => (
-  <InputMask
-    {...props}
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    formatChars={{
-      '9': '[0-9]',
-      a: '[A-Za-z]',
-      '*': '[A-Za-z0-9 .]',
-    }}
-    mask="+1 (999) 999-9999"
-    maskChar={null}
-    alwaysShowMask={false}
-    className={inputVariants({ className })}
-    inputRef={ref}
-  />
-));
+>(({ className, ...props }, ref) => {
+  const beforeMaskedValueChange = (newState: any) => {
+    let { value } = newState;
+    value = value.replace(/_+$/, '');
+    return {
+      ...newState,
+      value,
+    };
+  };
+  return (
+    <InputMask
+      {...props}
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      formatChars={{
+        '9': '[0-9]',
+        a: '[A-Za-z]',
+        '*': '[A-Za-z0-9 .]',
+      }}
+      mask="+1 (999) 999-9999"
+      maskChar={null}
+      alwaysShowMask={false}
+      beforeMaskedValueChange={beforeMaskedValueChange}
+      className={inputVariants({ className })}
+      inputRef={ref}
+    />
+  );
+});
 PhoneInput.displayName = 'PhoneInput';
