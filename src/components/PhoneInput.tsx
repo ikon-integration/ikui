9da1537acc6aaaacb1208798e16d56f090c6e699
drkedug@gmail.com <drkedug@gmail.com>
2024-06-04 14:11:49 -0300

    fix: appropriately config phone input

--- src/components/PhoneInput.tsx
+++ src/components/PhoneInput.tsx
@@ -25,7 +25,7 @@ export const PhoneInput = forwardRef<
         a: '[A-Za-z]',
         '*': '[A-Za-z0-9 .]',
       }}
-      mask="+1 (999) 999-9999 ******"
+      mask="+1 (999) 999-9999*********"
       alwaysShowMask={false}
       className={inputVariants({ className })}
       inputRef={ref}
