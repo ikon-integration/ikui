7b09885364c41d60dde6f7300abe0dc4fd64f069
drkedug@gmail.com <drkedug@gmail.com>
2024-07-20 00:18:44 -0300

    feat: improve css

--- src/components/SelectWithSearch.tsx
+++ src/components/SelectWithSearch.tsx
@@ -90,6 +90,7 @@ export function SelectWithSearch({
               <div
                 className={cn(
                   'ikui-flex ikui-h-10 ikui-w-full ikui-items-center ikui-justify-between ikui-rounded-md ikui-border ikui-border-input ikui-bg-background ikui-px-3 ikui-py-2 ikui-text-sm ikui-ring-offset-background ikui-transition-all placeholder:ikui-text-muted-foreground focus:ikui-outline-none focus:ikui-ring-2 focus:ikui-ring-ring focus:ikui-ring-offset-2 disabled:ikui-cursor-not-allowed disabled:ikui-opacity-50 [&>span]:ikui-line-clamp-1',
+                  'ikui-absolute ikui-left-2 ikui-flex ikui-h-3.5 ikui-w-3.5 ikui-items-center ikui-justify-center',
                   className,
                 )}
               >
@@ -99,8 +100,10 @@ export function SelectWithSearch({
                 autoSelect
                 placeholder="Type here..."
                 className={cn(
-                  'ikui-p-1',
-                  'ikui-h-[var(--radix-select-trigger-height)] ikui-w-full ikui-min-w-[var(--radix-select-trigger-width)]',
+                  'ikui-relative ikui-z-50 ikui-max-h-96 ikui-min-w-[8rem] ikui-overflow-hidden ikui-rounded-md ikui-border ikui-bg-popover ikui-text-popover-foreground ikui-shadow-md data-[state=open]:ikui-animate-in data-[state=closed]:ikui-animate-out data-[state=closed]:ikui-fade-out-0 data-[state=open]:ikui-fade-in-0 data-[state=closed]:ikui-zoom-out-95 data-[state=open]:ikui-zoom-in-95 data-[side=bottom]:ikui-slide-in-from-top-2 data-[side=left]:ikui-slide-in-from-right-2 data-[side=right]:ikui-slide-in-from-left-2 data-[side=top]:ikui-slide-in-from-bottom-2',
+                  'data-[side=bottom]:ikui-translate-y-1 data-[side=left]:ikui-translate-x-1 data-[side=right]:ikui-translate-x-1 data-[side=top]:ikui-translate-y-1',
+                  'ikui-h-10 ikui-pl-3',
+                  className,
                 )}
                 // Ariakit's Combobox manually triggers a blur event on virtually
                 // blurred items, making them work as if they had actual DOM
@@ -119,24 +122,20 @@ export function SelectWithSearch({
             </div>
             <ComboboxList className="overflow-y-auto p-1">
               {matches.map(({ label, value }) => (
-                <RadixSelect.Item
-                  key={value}
-                  value={value}
-                  asChild
-                  className="bg-green-500"
-                >
+                <RadixSelect.Item key={value} value={value} asChild>
+                  {/* 'ikui-relative ikui-flex ikui-w-full ikui-cursor-default ikui-select-none ikui-items-center ikui-rounded-sm ikui-py-1.5 ikui-pl-8 ikui-pr-2 ikui-text-sm ikui-outline-none focus:ikui-bg-accent focus:ikui-text-accent-foreground data-[disabled]:ikui-pointer-events-none data-[disabled]:ikui-opacity-50', */}
                   <ComboboxItem
                     className={cn(
                       'ikui-relative ikui-flex ikui-w-full ikui-cursor-default ikui-select-none ikui-items-center ikui-rounded-sm ikui-py-1.5 ikui-pl-8 ikui-pr-2 ikui-text-sm ikui-outline-none focus:ikui-bg-accent focus:ikui-text-accent-foreground data-[disabled]:ikui-pointer-events-none data-[disabled]:ikui-opacity-50',
                       className,
                     )}
                   >
-                    <RadixSelect.ItemText className="ikui-absolute ikui-left-2 ikui-flex ikui-h-3.5 ikui-w-3.5 ikui-items-center ikui-justify-center">
-                      {label}
-                    </RadixSelect.ItemText>
                     <RadixSelect.ItemIndicator className="item-indicator">
                       <CheckIcon className="ikui-h-4 ikui-w-4" />
                     </RadixSelect.ItemIndicator>
+                    <RadixSelect.ItemText className="ikui-absolute ikui-left-2 ikui-flex ikui-h-3.5 ikui-w-3.5 ikui-items-center ikui-justify-center">
+                      {label}
+                    </RadixSelect.ItemText>
                   </ComboboxItem>
                 </RadixSelect.Item>
               ))}
